a02b4dc10e4aa7e84ce2210f01d053aa0805b8c2
Yang Ke <saint_yk@163.com>
2017-03-11 11:55:46 +0800

    add some utility helper functions

--- resources/functions.php
+++ resources/functions.php
@@ -1,3 +1,26 @@
 <?php
 	
-	
+function redirect($location){
+	header("Location: ${location}");
+}
+
+function query($sql){
+	global $conn;
+	return msqli_query($conn,$sql);
+}
+
+function confirm($result){
+	global $conn;
+	if(!$result){
+		die("QUERY FAILED " . mysqli_error($conn));
+	}
+}
+
+function escape_string($string){
+	global $conn;
+	mysql_real_escape_string($conn,$string);
+}
+
+function fetch_array($send_query){
+	return mysqli_fetch_array($send_query);
+}
