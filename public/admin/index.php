4cb2ce264096bab123707d704b207a25a6e8ed2b
Yang Ke <saint_yk@163.com>
2017-05-06 17:56:42 +0800

    setting up the rest of the pages

--- public/admin/index.php
+++ public/admin/index.php
@@ -30,6 +30,22 @@
                     if(isset($_GET['orders'])){
                         include(TEMPLATE_BACK . DS . 'orders.php');
                     }
+
+                    if(isset($_GET['categories'])){
+                        include(TEMPLATE_BACK . DS . 'categories.php');
+                    }
+
+                    if(isset($_GET['products'])){
+                        include(TEMPLATE_BACK . DS . 'products.php');
+                    }
+
+                    if(isset($_GET['add_product'])){
+                        include(TEMPLATE_BACK . DS . 'add_product.php');
+                    }
+
+                    if(isset($_GET['users'])){
+                        include(TEMPLATE_BACK . DS . 'users.php');
+                    }
                  ?>
 
             </div>
