3fa3ab9f245671ad1fbfba3b18b9f28c9a619025
Yang Ke <saint_yk@163.com>
2017-05-09 22:00:22 +0800

    define UPLOAD_DIRECTORY constant

--- resources/config.php
+++ resources/config.php
@@ -6,6 +6,7 @@ session_start();
 defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR);
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT",__DIR__ . DS . "templates/front");
 defined("TEMPALTE_BACK") ? null : define("TEMPLATE_BACK",__DIR__ . DS . "templates/back");
+defined("UPLOAD_DIRECTORY") ? null : define("UPLOAD_DIRECTORY",__DIR__ . DS. "uploads");
 // db 
 defined("DB_HOST") ? null : define("DB_HOST","localhost");
 defined("DB_USER") ? null : define("DB_USER","root");
